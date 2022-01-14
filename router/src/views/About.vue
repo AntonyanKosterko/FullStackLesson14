aa6e4b37b6a03232d3031afb2f9ef7d3de28bf64
Your Name <your_email@whatever.com>
2022-01-14 19:18:14 +0300

    Add vue-router, create simple application with routing

---
+++ router/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Тут мы что-то объясняем</h1>
+  </div>
+</template>
---
+++ router/src/views/Contacts.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Здесь можно с нами связаться</h1>
+  </div>
+</template>
